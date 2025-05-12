4098fa8347c92bdea91dfccc10c8c10e7ae74129
oniafk <manufelipearias@gmail.com>
2025-05-12 23:20:00 +1000

    grid priduct section

---
+++ src/slices/ProductGrid/index.tsx
@@ -0,0 +1,48 @@
+import { Content, isFilled } from "@prismicio/client";
+import {
+  PrismicRichText,
+  PrismicText,
+  SliceComponentProps,
+} from "@prismicio/react";
+
+import { Bounded } from "@/components/Bounded";
+import { Heading } from "@/components/Heading";
+import { SkateboardProduct } from "./SkateboardProduct";
+import { JSX } from "react";
+// import { SlideIn } from "@/components/SlideIn";
+
+export type ProductGridProps = SliceComponentProps<Content.ProductGridSlice>;
+
+/**
+ * Component for "ProductGrid" Slices.
+ */
+const ProductGrid = ({ slice }: ProductGridProps): JSX.Element => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="bg-texture bg-brand-gray"
+    >
+      {/* <SlideIn> */}
+      <Heading className="text-center ~mb-4/6" as="h2">
+        <PrismicText field={slice.primary.heading} />
+      </Heading>
+      {/* </SlideIn> */}
+      {/* <SlideIn> */}
+      <div className="text-center ~mb-6/10">
+        <PrismicRichText field={slice.primary.body} />
+      </div>
+      {/* </SlideIn> */}
+      <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+        {slice.primary.product.map(
+          ({ skateboards }) =>
+            isFilled.contentRelationship(skateboards) && (
+              <SkateboardProduct key={skateboards.id} id={skateboards.id} />
+            )
+        )}
+      </div>
+    </Bounded>
+  );
+};
+
+export default ProductGrid;
