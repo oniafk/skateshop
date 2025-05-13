c3b6753559e0de38bcdf0e1e7d68e1030d05ea35
oniafk <manufelipearias@gmail.com>
2025-05-14 09:07:01 +1000

    set context for getting info

--- src/app/build/context.tsx
+++ src/app/build/context.tsx
@@ -3,22 +3,58 @@
 import { createContext, ReactNode, useContext, useMemo, useState } from "react";
 import { Content } from "@prismicio/client";
 
-type CustomizerControlsContext = {};
+type CustomizerControlsContext = {
+  selectedWheel?: Content.BoardCustomizerDocumentDataWheelsItem;
+  setWheel: (wheel: Content.BoardCustomizerDocumentDataWheelsItem) => void;
+  selectedDeck?: Content.BoardCustomizerDocumentDataDecksItem;
+  setDeck: (deck: Content.BoardCustomizerDocumentDataDecksItem) => void;
+  selectedTruck?: Content.BoardCustomizerDocumentDataMetalsItem;
+  setTruck: (trucks: Content.BoardCustomizerDocumentDataMetalsItem) => void;
+  selectedBolt?: Content.BoardCustomizerDocumentDataMetalsItem;
+  setBolt: (bolts: Content.BoardCustomizerDocumentDataMetalsItem) => void;
+};
 
-const defaultContext: CustomizerControlsContext = {};
+const defaultContext: CustomizerControlsContext = {
+  setWheel: () => {},
+  setDeck: () => {},
+  setTruck: () => {},
+  setBolt: () => {},
+};
 
 const CustomizerControlsContext = createContext(defaultContext);
 
 type CustomizerControlsProviderProps = {
+  defaultWheel?: Content.BoardCustomizerDocumentDataWheelsItem;
+  defaultDeck?: Content.BoardCustomizerDocumentDataDecksItem;
+  defaultTruck?: Content.BoardCustomizerDocumentDataMetalsItem;
+  defaultBolt?: Content.BoardCustomizerDocumentDataMetalsItem;
   children?: ReactNode;
 };
 
 export function CustomizerControlsProvider({
+  defaultWheel,
+  defaultDeck,
+  defaultTruck,
+  defaultBolt,
   children,
 }: CustomizerControlsProviderProps) {
+  const [selectedWheel, setWheel] = useState(defaultWheel);
+  const [selectedDeck, setDeck] = useState(defaultDeck);
+  const [selectedTruck, setTruck] = useState(defaultTruck);
+  const [selectedBolt, setBolt] = useState(defaultBolt);
+
   const value = useMemo(() => {
-    return {};
-  }, []);
+    return {
+      selectedWheel,
+      setWheel,
+      selectedDeck,
+      setDeck,
+      selectedTruck,
+      setTruck,
+      selectedBolt,
+      setBolt,
+    };
+  }, [selectedWheel, selectedDeck, selectedTruck, selectedBolt]);
 
   return (
     <CustomizerControlsContext.Provider value={value}>
